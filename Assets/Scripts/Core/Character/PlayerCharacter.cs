61024786842b55bb86bf65fc00b973f854884fcf
rStar <starlove124760@gmail.com>
2022-11-05 08:59:58 +0800

    add folders and prefab

---
+++ Assets/Scripts/Core/Character/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+namespace Core
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
+    #region Private Variables
+
+
+    #endregion
+
+    #region Unity events
+
+        private void Update()
+        {
+
+        }
+
+    #endregion
+    }
+}
---
+++ Assets/Scripts/StateMachine Practice/NPCStateMachine.cs
@@ -0,0 +1,32 @@
+#region
+
+using FSM;
+using UnityEngine;
+
+#endregion
+
+public enum NPCState
+{
+    IDLE , WALK
+}
+
+public class NPCStateMachine : MonoBehaviour
+{
+#region Private Variables
+
+
+#endregion
+
+#region Unity events
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
+
+#endregion
+}
