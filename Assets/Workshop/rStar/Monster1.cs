61911271bf2228cd452f64e5435c90f48e0e0d9e
rStar <starlove124760@gmail.com>
2022-11-06 12:22:11 +0800

    add practice amsdef and rstar folder

---
+++ Assets/Workshop/rStar/Monster1.cs
@@ -0,0 +1,22 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+public class Monster1 : MonoBehaviour
+{
+#region Unity events
+
+    // Start is called before the first frame update
+    private void Start() { }
+
+#endregion
+
+#region Private Methods
+
+    // Update is called once per frame
+    private void Update() { }
+
+#endregion
+}
