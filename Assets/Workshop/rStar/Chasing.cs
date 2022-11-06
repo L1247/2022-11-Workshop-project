69368b75b7c05b28f3b8b07811b7bcb342dcd0d0
rStar <starlove124760@gmail.com>
2022-11-06 15:51:06 +0800

    add chasing by star

---
+++ Assets/Workshop/rStar/Chasing.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using FSM;
+using UnityEngine;
+
+#endregion
+
+class Chasing : State<string>
+{
+#region Private Variables
+
+    private readonly Animator  animator;
+    private readonly float     moveSpeed;
+    private readonly string    chasingStateName = "Walk";
+    private readonly Transform self;
+    private readonly Transform target;
+
+#endregion
+
+#region Constructor
+
+    public Chasing(Transform self , Transform target , Animator animator , float moveSpeed)
+    {
+        this.self      = self;
+        this.target    = target;
+        this.animator  = animator;
+        this.moveSpeed = moveSpeed;
+    }
+
+#endregion
+
+#region Events
+
+    public override void OnEnter()
+    {
+        animator.Play(chasingStateName);
+    }
+
+    public override void OnLogic()
+    {
+        var moveDirection = (target.position - self.position).normalized;
+        var movement      = moveDirection * Time.deltaTime * moveSpeed;
+        self.position += movement;
+    }
+
+#endregion
+}
--- Assets/Workshop/rStar/Monster1.cs
+++ Assets/Workshop/rStar/Monster1.cs
@@ -1,22 +1,48 @@
 #region
 
+using FSM;
 using UnityEngine;
 
 #endregion
 
 public class Monster1 : MonoBehaviour
 {
+#region Private Variables
+
+    private StateMachine<string> fsm;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private Transform target;
+
+#endregion
+
 #region Unity events
 
     // Start is called before the first frame update
-    private void Start() { }
+    private void Start()
+    {
+        var chasing = "Chasing";
+        fsm = new StateMachine<string>();
+        fsm.AddState(chasing , new Chasing(transform , target , animator , moveSpeed));
+        fsm.SetStartState(chasing);
+        fsm.Init();
+    }
 
 #endregion
 
 #region Private Methods
 
     // Update is called once per frame
-    private void Update() { }
+    private void Update()
+    {
+        fsm.OnLogic();
+    }
 
 #endregion
 }
