e3d348a2888e95c7c6ad321dc6b45e0ded1fcc9e
Chika Chang <13810485+chikastory@users.noreply.github.com>
2022-11-06 14:44:10 +0800

    feat(team4): add player and monster

---
+++ Assets/Workshop/Team4/Scripts/Death.cs
@@ -0,0 +1,12 @@
+﻿using FSM;
+using UnityEngine;
+
+class Death : State<Monster.State> {
+    GameObject _self;
+    public Death(GameObject gameObject) => _self = gameObject;
+
+    public override void OnEnter() {
+        base.OnEnter();
+        Object.Destroy(_self);
+    }
+}
---
+++ Assets/Workshop/Team4/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Scripts {
+    public class Player : MonoBehaviour {
+        // public void ShootClosetMonster() {
+        //     FindObjectsOfType<Monster>().FirstOrDefault(x => x)
+        // }
+    }
+}
