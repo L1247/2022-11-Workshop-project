1b7211e58b2651ec712f9e768da25a72a1b285a5
Jimi <jingmin@toydea.com>
2022-11-06 13:50:55 +0900

    feat: 【team3】 moster statemachine

---
+++ Assets/Workshop/Team3/Chase.cs
@@ -0,0 +1,20 @@
+﻿
+using FSM;
+using UnityEngine;
+
+namespace Workshop.Team3
+{
+    public class Chase :State<Moster1StateMachine.State>
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Debug.Log("Chase");
+        }
+
+        public override void OnLogic()
+        {
+            base.OnLogic();
+        }
+    }
+}
