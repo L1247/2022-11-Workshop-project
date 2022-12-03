6df2b92748e74b8a9495aa449000991ac1821183
r Star <starlove124760@gmail.com>
2022-12-03 12:19:01 +0800

    add testcases for _01_MoveTowardsTarget

--- Assets/GameJam/Core.Tests/ChasingTests.cs
+++ Assets/GameJam/Core.Tests/ChasingTests.cs
@@ -10,25 +10,34 @@ public class ChasingTests
 #region Test Methods
 
     [Test]
-    public void _01_MoveTowardsTarget()
+    [TestCase(1.42f ,  1.42f ,  0.71f ,  0.71f)]
+    [TestCase(-1.42f , -1.42f , -0.71f , -0.71f)]
+    public void _01_MoveTowardsTarget(float targetPosX , float targetPosY , float frame1X , float frame1Y)
     {
         var self = new GameObject("Self").transform;
         self.position = Vector3.zero;
-        var target = new GameObject("Target").transform;
-        target.position = new Vector3(1.42f , 1.42f , 0);
+        var target         = new GameObject("Target").transform;
+        var targetPosition = Given_Pos(targetPosX , targetPosY);
+        target.position = targetPosition;
         Animator animator = null;
         var      chasing  = new Chasing(self , target , animator , 1);
 
         chasing.OnLogic();
-        Should_Position_Equal(self.position , 0.71f , 0.71f);
+        Should_Position_Equal(self.position , frame1X , frame1Y);
 
         chasing.OnLogic();
-        Should_Position_Equal(self.position , 1.42f , 1.42f);
+        Should_Position_Equal(self.position , targetPosX , targetPosY);
     }
 
 #endregion
 
 #region Private Methods
+
+    private static Vector3 Given_Pos(float x , float y)
+    {
+        var targetPosition = new Vector3(x , y , 0);
+        return targetPosition;
+    }
 
     private static void Should_Position_Equal(Vector3 pos , float exceptedX , float exceptedY)
     {
