4415088c4d2c6a96444bd0725a3c54ec0dbc17e2
r Star <starlove124760@gmail.com>
2022-12-04 03:48:39 +0800

    add Description for tests

--- Assets/GameJam/Core.Tests/ChasingTests.cs
+++ Assets/GameJam/Core.Tests/ChasingTests.cs
@@ -8,98 +8,98 @@ using UnityEngine;
 
 public class ChasingTests
 {
-    #region Test Methods
+#region Test Methods
 
-    [Test]
-    [TestCase(-1.42f, -1.42f, -0.71f, -0.71f, Facing.Right,
-        Facing.Left, Description = "左")]
-    [TestCase(1.42f, 1.42f, 0.71f, 0.71f, Facing.Left,
-        Facing.Right, Description = "右")]
+    [Test(Description = "往目標方向移動")]
+    [TestCase(                              -1.42f , -1.42f , -0.71f , -0.71f , Facing.Right ,
+                             Facing.Left ,  Description = "左")]
+    [TestCase(                              1.42f , 1.42f , 0.71f , 0.71f , Facing.Left ,
+                             Facing.Right , Description = "右")]
     [Category("Moving")]
-    public void _01_MoveTowardsTarget(float targetPosX, float targetPosY, float frame1X, float frame1Y,
-        Facing defaultFacing,
-        Facing expectedFacing)
+    public void _01_MoveTowardsTarget(float  targetPosX , float targetPosY , float frame1X , float frame1Y ,
+                                      Facing defaultFacing ,
+                                      Facing expectedFacing)
     {
-        var self = Given_A_Monster1_With_Pos(Given_Pos(0, 0));
-        Given_A_Facing(self, defaultFacing);
-        var target = Given_A_Monster1_With_Pos(Given_Pos(targetPosX, targetPosY));
-        var chasing = Given_A_Chasing_State(self, target.transform);
+        var self = Given_A_Monster1_With_Pos(Given_Pos(0 , 0));
+        Given_A_Facing(self , defaultFacing);
+        var target  = Given_A_Monster1_With_Pos(Given_Pos(targetPosX , targetPosY));
+        var chasing = Given_A_Chasing_State(self , target.transform);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), frame1X, frame1Y);
+        Should_Position_Equal(self.GetPos() , frame1X , frame1Y);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), targetPosX, targetPosY);
+        Should_Position_Equal(self.GetPos() , targetPosX , targetPosY);
 
-        Should_Facing_Is(self, expectedFacing);
+        Should_Facing_Is(self , expectedFacing);
     }
 
 
-    [Test]
-    [TestCase(-1, -1, -0.99f, -0.99f, Facing.Left,
-        Description = "左")]
-    [TestCase(1, 1, 0.99f, 0.99f, Facing.Right,
-        Description = "右")]
+    [Test(Description = "距離靠近，停止移動")]
+    [TestCase(-1 , -1 , -0.99f , -0.99f , Facing.Left ,
+              Description = "左")]
+    [TestCase(1 , 1 , 0.99f , 0.99f , Facing.Right ,
+              Description = "右")]
     [Category("Moving")]
-    public void _02_StopMoving(float targetPosX, float targetPosY, float selfPosX, float selfPosY, Facing facing)
+    public void _02_StopMoving(float targetPosX , float targetPosY , float selfPosX , float selfPosY , Facing facing)
     {
-        var self = Given_A_Monster1_With_Pos(Given_Pos(selfPosX, selfPosY));
-        Given_A_Facing(self, facing);
-        var target = Given_A_Monster1_With_Pos(Given_Pos(targetPosY, targetPosY));
-        var chasing = Given_A_Chasing_State(self, target.transform);
+        var self = Given_A_Monster1_With_Pos(Given_Pos(selfPosX , selfPosY));
+        Given_A_Facing(self , facing);
+        var target  = Given_A_Monster1_With_Pos(Given_Pos(targetPosY , targetPosY));
+        var chasing = Given_A_Chasing_State(self , target.transform);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), selfPosX, selfPosY);
-        Should_Facing_Is(self, facing);
+        Should_Position_Equal(self.GetPos() , selfPosX , selfPosY);
+        Should_Facing_Is(self , facing);
     }
 
-    [Test]
-    [TestCase(-1.42f, -1.42f, -1.42f, -1.42f, Facing.Right,
-        Facing.Left, Description = "左")]
-    [TestCase(1.42f, 1.42f, 1.42f, 1.42f, Facing.Left,
-        Facing.Right, Description = "右")]
+    [Test(Description = "移動速度過快")]
+    [TestCase(                              -1.42f , -1.42f , -1.42f , -1.42f , Facing.Right ,
+                             Facing.Left ,  Description = "左")]
+    [TestCase(                              1.42f , 1.42f , 1.42f , 1.42f , Facing.Left ,
+                             Facing.Right , Description = "右")]
     [Category("Moving")]
-    public void _03_MoveSpeed_Is_High(float targetPosX, float targetPosY, float frame1X, float frame1Y,
-        Facing defaultFacing,
-        Facing expectedFacing)
+    public void _03_MoveSpeed_Is_High(float  targetPosX , float targetPosY , float frame1X , float frame1Y ,
+                                      Facing defaultFacing ,
+                                      Facing expectedFacing)
     {
-        var self = Given_A_Monster1_With_Pos(Given_Pos(0, 0));
-        Given_A_Facing(self, defaultFacing);
-        var target = Given_A_Monster1_With_Pos(Given_Pos(targetPosX, targetPosY));
-        var chasing = Given_A_Chasing_State(self, target.transform, 9999);
+        var self = Given_A_Monster1_With_Pos(Given_Pos(0 , 0));
+        Given_A_Facing(self , defaultFacing);
+        var target  = Given_A_Monster1_With_Pos(Given_Pos(targetPosX , targetPosY));
+        var chasing = Given_A_Chasing_State(self , target.transform , 9999);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), frame1X, frame1Y);
+        Should_Position_Equal(self.GetPos() , frame1X , frame1Y);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), targetPosX, targetPosY);
+        Should_Position_Equal(self.GetPos() , targetPosX , targetPosY);
 
-        Should_Facing_Is(self, expectedFacing);
+        Should_Facing_Is(self , expectedFacing);
     }
 
     [Test(Description = "追擊_無目標")]
     public void _04_Move_With_No_Target()
     {
-        var self = Given_A_Monster1_With_Pos(Given_Pos(0,0));
-        Given_A_Facing(self, Facing.Right);
-        var chasing = Given_A_Chasing_State(self, null, 1);
-        
+        var self = Given_A_Monster1_With_Pos(Given_Pos(0 , 0));
+        Given_A_Facing(self , Facing.Right);
+        var chasing = Given_A_Chasing_State(self , null , 1);
+
         UpdateTheState(chasing);
-        Should_Position_Equal(self.GetPos(), 0,0);
+        Should_Position_Equal(self.GetPos() , 0 , 0);
     }
 
-    #endregion
+#endregion
 
-    #region Private Methods
+#region Private Methods
 
-    private static Chasing Given_A_Chasing_State(Monster1 self, Transform target, float moveSpeed = 1)
+    private static Chasing Given_A_Chasing_State(Monster1 self , Transform target , float moveSpeed = 1)
     {
-        var chasing = new Chasing(self, target, null, moveSpeed);
+        var chasing = new Chasing(self , target , null , moveSpeed);
         chasing.SetDeltaTime(1);
         return chasing;
     }
 
-    private static void Given_A_Facing(Monster1 self, Facing facing)
+    private static void Given_A_Facing(Monster1 self , Facing facing)
     {
         self.SetFacing(facing);
     }
@@ -111,23 +111,23 @@ public class ChasingTests
         return monster1;
     }
 
-    private static Vector3 Given_Pos(float x, float y)
+    private static Vector3 Given_Pos(float x , float y)
     {
-        var targetPosition = new Vector3(x, y, 0);
+        var targetPosition = new Vector3(x , y , 0);
         return targetPosition;
     }
 
-    private static void Should_Facing_Is(Monster1 monster1, Facing exceptedFacing)
+    private static void Should_Facing_Is(Monster1 monster1 , Facing exceptedFacing)
     {
-        Assert.AreEqual(exceptedFacing, monster1.GetFacing());
+        Assert.AreEqual(exceptedFacing , monster1.GetFacing());
     }
 
-    private static void Should_Position_Equal(Vector3 pos, float exceptedX, float exceptedY)
+    private static void Should_Position_Equal(Vector3 pos , float exceptedX , float exceptedY)
     {
         var posX = pos.x;
         var posY = pos.y;
-        Assert.AreEqual(exceptedX, posX, 0.01f, "x is not equal.");
-        Assert.AreEqual(exceptedY, posY, 0.01f, "y is not equal.");
+        Assert.AreEqual(exceptedX , posX , 0.01f , "x is not equal.");
+        Assert.AreEqual(exceptedY , posY , 0.01f , "y is not equal.");
     }
 
     private static void UpdateTheState(Chasing chasing)
@@ -135,5 +135,5 @@ public class ChasingTests
         chasing.OnLogic();
     }
 
-    #endregion
+#endregion
 }
--- Assets/GameJam/Core.Tests/Monster1Tests.cs
+++ Assets/GameJam/Core.Tests/Monster1Tests.cs
@@ -11,25 +11,35 @@ public class Monster1Tests
 {
 #region Test Methods
 
-    [Test]
-    [TestCase(Facing.Right , 1)]
-    [TestCase(Facing.Left ,  -1)]
-    public void Monster_Facing(Facing facing , float expectedScaleX)
+    [Test(Description = "不能設定怪物面向為None")]
+    public void Monster_Cant_Facing_None()
     {
-        var monster1 = new GameObject("").AddComponent<Monster1>();
-        monster1.SetFacing(facing);
-        Assert.AreEqual(expectedScaleX , monster1.transform.localScale.x);
-    }
-
-    [Test]
-    public void Monster_Facing_None()
-    {
-        var monster1 = new GameObject("").AddComponent<Monster1>();
+        var monster1 = Given_A_Monster1();
         monster1.SetFacing(Facing.Left);
 
         Assert.DoesNotThrow(() => monster1.SetFacing(Facing.None));
 
         Assert.AreEqual(Facing.Left , monster1.GetFacing());
+    }
+
+    [Test(Description = "怪物面向")]
+    [TestCase(Facing.Right , 1)]
+    [TestCase(Facing.Left ,  -1)]
+    public void Monster_Facing(Facing facing , float expectedScaleX)
+    {
+        var monster1 = Given_A_Monster1();
+        monster1.SetFacing(facing);
+        Assert.AreEqual(expectedScaleX , monster1.transform.localScale.x);
+    }
+
+#endregion
+
+#region Private Methods
+
+    private static Monster1 Given_A_Monster1()
+    {
+        var monster1 = new GameObject("").AddComponent<Monster1>();
+        return monster1;
     }
 
 #endregion
