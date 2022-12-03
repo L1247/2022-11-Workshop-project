cc49187e0155f3c90cdde89d5f13b50d76665f42
r Star <starlove124760@gmail.com>
2022-12-03 10:54:08 +0800

    add gamejam folder

---
+++ Assets/GameJam/Core/Chasing.cs
@@ -0,0 +1,19 @@
+#region
+
+using System;
+using FSM;
+using UnityEngine;
+
+#endregion
+
+internal class Chasing : State<string>
+{
+#region Constructor
+
+    public Chasing(Transform transform , Transform target , Animator animator , float moveSpeed)
+    {
+        throw new NotImplementedException();
+    }
+
+#endregion
+}
