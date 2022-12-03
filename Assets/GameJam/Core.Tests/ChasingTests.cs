d1c5e8051f889a78a2f66dbd76ac4cf1d8740c1f
r Star <starlove124760@gmail.com>
2022-12-03 12:22:59 +0800

    refactor clean code

--- Assets/GameJam/Core.Tests/ChasingTests.cs
+++ Assets/GameJam/Core.Tests/ChasingTests.cs
@@ -14,24 +14,27 @@ public class ChasingTests
     [TestCase(-1.42f , -1.42f , -0.71f , -0.71f)]
     public void _01_MoveTowardsTarget(float targetPosX , float targetPosY , float frame1X , float frame1Y)
     {
-        var self = new GameObject("Self").transform;
-        self.position = Vector3.zero;
-        var target         = new GameObject("Target").transform;
-        var targetPosition = Given_Pos(targetPosX , targetPosY);
-        target.position = targetPosition;
-        Animator animator = null;
-        var      chasing  = new Chasing(self , target , animator , 1);
+        var self    = Given_A_Transform_With_Pos(Given_Pos(0 ,          0));
+        var target  = Given_A_Transform_With_Pos(Given_Pos(targetPosX , targetPosY));
+        var chasing = new Chasing(self , target , null , 1);
 
-        chasing.OnLogic();
+        UpdateTheState(chasing);
         Should_Position_Equal(self.position , frame1X , frame1Y);
 
-        chasing.OnLogic();
+        UpdateTheState(chasing);
         Should_Position_Equal(self.position , targetPosX , targetPosY);
     }
 
 #endregion
 
 #region Private Methods
+
+    private static Transform Given_A_Transform_With_Pos(Vector3 pos)
+    {
+        var self = new GameObject("Self").transform;
+        self.position = pos;
+        return self;
+    }
 
     private static Vector3 Given_Pos(float x , float y)
     {
@@ -45,6 +48,11 @@ public class ChasingTests
         var posY = pos.y;
         Assert.AreEqual(exceptedX , posX , 0.01f , "x is not equal.");
         Assert.AreEqual(exceptedY , posY , 0.01f , "y is not equal.");
+    }
+
+    private static void UpdateTheState(Chasing chasing)
+    {
+        chasing.OnLogic();
     }
 
 #endregion
