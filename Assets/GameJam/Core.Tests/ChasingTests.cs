35706ecb507cdfc138d0b98ee556f6ca06872967
Plumes1227 <qfbnhg56548x@gmail.com>
2022-12-03 12:37:28 +0800

    tast facing

--- Assets/GameJam/Core.Tests/ChasingTests.cs
+++ Assets/GameJam/Core.Tests/ChasingTests.cs
@@ -7,27 +7,28 @@ using UnityEngine;
 
 public class ChasingTests
 {
-#region Test Methods
+    #region Test Methods
 
     [Test]
-    [TestCase(1.42f ,  1.42f ,  0.71f ,  0.71f)]
-    [TestCase(-1.42f , -1.42f , -0.71f , -0.71f)]
-    public void _01_MoveTowardsTarget(float targetPosX , float targetPosY , float frame1X , float frame1Y)
+    [TestCase(1.42f, 1.42f, 0.71f, 0.71f,1f)]
+    [TestCase(-1.42f, -1.42f, -0.71f, -0.71f,-1f)]
+    public void _01_MoveTowardsTarget(float targetPosX, float targetPosY, float frame1X, float frame1Y, float scaleX)
     {
-        var self    = Given_A_Transform_With_Pos(Given_Pos(0 ,          0));
-        var target  = Given_A_Transform_With_Pos(Given_Pos(targetPosX , targetPosY));
-        var chasing = new Chasing(self , target , null , 1);
+        var self = Given_A_Transform_With_Pos(Given_Pos(0, 0));
+        var target = Given_A_Transform_With_Pos(Given_Pos(targetPosX, targetPosY));
+        var chasing = new Chasing(self, target, null, 1);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.position , frame1X , frame1Y);
+        Should_Position_Equal(self.position, frame1X, frame1Y);
 
         UpdateTheState(chasing);
-        Should_Position_Equal(self.position , targetPosX , targetPosY);
+        Should_Position_Equal(self.position, targetPosX, targetPosY);
+        Assert.AreEqual(scaleX, self.localScale.x);
     }
 
-#endregion
+    #endregion
 
-#region Private Methods
+    #region Private Methods
 
     private static Transform Given_A_Transform_With_Pos(Vector3 pos)
     {
@@ -36,18 +37,18 @@ public class ChasingTests
         return self;
     }
 
-    private static Vector3 Given_Pos(float x , float y)
+    private static Vector3 Given_Pos(float x, float y)
     {
-        var targetPosition = new Vector3(x , y , 0);
+        var targetPosition = new Vector3(x, y, 0);
         return targetPosition;
     }
 
-    private static void Should_Position_Equal(Vector3 pos , float exceptedX , float exceptedY)
+    private static void Should_Position_Equal(Vector3 pos, float exceptedX, float exceptedY)
     {
         var posX = pos.x;
         var posY = pos.y;
-        Assert.AreEqual(exceptedX , posX , 0.01f , "x is not equal.");
-        Assert.AreEqual(exceptedY , posY , 0.01f , "y is not equal.");
+        Assert.AreEqual(exceptedX, posX, 0.01f, "x is not equal.");
+        Assert.AreEqual(exceptedY, posY, 0.01f, "y is not equal.");
     }
 
     private static void UpdateTheState(Chasing chasing)
@@ -55,5 +56,5 @@ public class ChasingTests
         chasing.OnLogic();
     }
 
-#endregion
+    #endregion
 }
