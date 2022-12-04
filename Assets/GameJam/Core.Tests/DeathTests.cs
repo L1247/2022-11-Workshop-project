df887522be0841f81b22cf98139b275b10a7e1a9
r Star <starlove124760@gmail.com>
2022-12-04 16:02:15 +0800

    test: _01_Disappear_On_Enter

--- Assets/GameJam/Core.Tests/DeathTests.cs
+++ Assets/GameJam/Core.Tests/DeathTests.cs
@@ -2,21 +2,37 @@
 
 using GameJam.Core.States;
 using NUnit.Framework;
+using UnityEngine;
 
 #endregion
 
 [TestFixture]
 public class DeathTests : GGJTests
 {
+#region Setup/Teardown Methods
+
+    [TearDown]
+    public void CleanUp()
+    {
+        foreach (var gameObject in Object.FindObjectsOfType<GameObject>())
+            Object.DestroyImmediate(gameObject);
+    }
+
+#endregion
+
 #region Test Methods
 
     [Test(Description = "進入死亡，消失")]
     public void _01_Disappear_On_Enter()
     {
         var monsterA = Give_A_Monster1();
-        new Death(monsterA).OnEnter();
-        var targetB = Give_A_Monster1();
-        Assert.IsNull(monsterA);
+        var targetB  = Give_A_Monster1();
+        var death    = new Death(monsterA);
+        death.OnEnter();
+
+        var monster1s = Object.FindObjectsOfType<Monster1>();
+        Assert.AreEqual(1 , monster1s.Length);
+        Assert.AreEqual(targetB , monster1s[0]);
     }
 
 #endregion
--- Assets/GameJam/Core/States/Death.cs
+++ Assets/GameJam/Core/States/Death.cs
@@ -9,16 +9,28 @@ namespace GameJam.Core.States
 {
     public class Death : State<string>
     {
+    #region Private Variables
+
         private readonly Monster1 monsterA;
+
+    #endregion
+
+    #region Constructor
 
         public Death(Monster1 monsterA)
         {
             this.monsterA = monsterA;
         }
 
+    #endregion
+
+    #region Public Methods
+
         public override void OnEnter()
         {
-            GameObject.DestroyImmediate(monsterA.gameObject);
+            Object.DestroyImmediate(monsterA.gameObject);
         }
+
+    #endregion
     }
 }
